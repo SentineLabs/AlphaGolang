4d11943ed62e286a9546d11e28a8bf62c7d86e6a
turla <jags.sec@gmail.com>
2021-10-02 21:57:22 -0400

    Heavily refactored categorize_go_folders.py with great results Added a README to credit folks that helped along the way :)

--- categorize_go_folders.py
+++ categorize_go_folders.py
@@ -5,53 +5,49 @@ import ida_dirtree
 import idc
 
 func_dir: ida_dirtree.dirtree_t
-func_dir = ida_dirtree.get_std_dirtree(ida_dirtree.DIRTREE_FUNCS)
+func_dir =   ida_dirtree.get_std_dirtree(ida_dirtree.DIRTREE_FUNCS)
 ite = ida_dirtree.dirtree_iterator_t()
 ok = func_dir.findfirst(ite, "*")
 
 folders = {}
-uncategorized = []
-
 for function in idautils.Functions():
     name = idc.get_func_name(function)
-    try:
-        index = name.index("_")
-        index2 = name.index(".")
-        if index2 < index:
-            index = index2
-        folderName = name[:index]
-    except:
-        uncategorized.append(name)
+    folderName = ""
+    if name.startswith("_") or name.startswith("."):
+        folders.setdefault("uncategorized",[]).append(name)
         continue
-    if folderName in folders:
-        folders[folderName].append(name)
-    else:
-        folders[folderName] = [name]
+    for i,c in enumerate(name):
+        if c == "." or c == "_": 
+            folderName = name[:i]
+            break
+    if folderName == "":
+        folderName = "uncategorized"
+    folders.setdefault(folderName,[]).append(name)
 for folderName in folders:
-        try:
-            func_dir.mkdir(folderName)
-            #print("Created folder: ", folderName)    
-        except:
-            print("Failed to create folder: ", folderName)
-            continue
-        for name in folders[folderName]:
-            try:
-                func_dir.rename(name, folderName+"/"+name)
-                #print("Moved: "+name+" to "+folderName)
-            except:
-                print("Failed to move function:", name)
-
-folderName = "uncategorized"
-try:
-    func_dir.mkdir(folderName)
-    #print("Created folder: ", folderName)    
-    for name in uncategorized:
+    try:
+        func_dir.mkdir(folderName)
+    except:
+        print("Failed to create folder: ", folderName)
+        continue
+    for name in folders[folderName]:
         try:
             func_dir.rename(name, folderName+"/"+name)
             #print("Moved: "+name+" to "+folderName)
         except:
-            print("Failed to move uncategorized function:", name)
+            print("Failed to move function:", name)
+
+common_packages = ["archive", "bufio", "builtin", "bytes", "compress", "container", "context", "crypto", "database", "debug", "embed", "encoding", "errors", "flag", "fmt", "go", "hash", "html", "image", "index", "io", "log", "math", "mime", "net", "os", "path", "plugin", "regexp", "sort", "strconv", "strings", "sync", "syscall", "testing", "text", "time", "unicode", "unsafe", "internal", "reflect", "vendor", "golang", "runtime", "type"]
+
+folderName = "StandardGoPackages"
+
+try:
+    func_dir.mkdir(folderName)
+    #print("Created folder: ", folderName)    
+    for package in common_packages:
+        try:
+            func_dir.rename(package, folderName+"/"+package)
+            #print("Moved: "+name+" to "+folderName)
+        except:
+            print("Failed to move standard package folder:", package)
 except:
     print("Failed to create folder: ", folderName)
-    
-
