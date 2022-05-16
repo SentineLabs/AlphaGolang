998ac3f748ea7a3714f7290899db4fd45b361f2c
turla <jags.sec@gmail.com>
2022-05-16 17:57:06 -0400

    asd# Added support for IDA 7.7+ in Step 3, resolving conflict with native folder sorting
    Added half-assed support for non-windows Go binaries in Step 5

--- 3.categorize_go_folders.py
+++ 3.categorize_go_folders.py
@@ -10,51 +10,57 @@ func_dir =   ida_dirtree.get_std_dirtree(ida_dirtree.DIRTREE_FUNCS)
 ite = ida_dirtree.dirtree_iterator_t()
 ok = func_dir.findfirst(ite, "*")
 
+version770plus = False
 folders = {}
-for function in idautils.Functions():
-    name = idc.get_func_name(function)
-    folderName = ""
-    if name.startswith("_") or name.startswith("."):
-        folders.setdefault("uncategorized",[]).append(name)
-        continue
-    for i,c in enumerate(name):
-        if c == "." or c == "_": 
-            folderName = name[:i]
-            break
-    if folderName == "":
-        folderName = "uncategorized"
-    if folderName == "sub":
-        folderName = "unnamed"
-    folders.setdefault(folderName,[]).append(name)
-for folderName in folders:
+folderName = ""
+
+def populate_folders():
+    for function in idautils.Functions():
+        name = idc.get_func_name(function)
+        if name.startswith("_") or name.startswith("."):
+            folders.setdefault("uncategorized",[]).append(name)
+            continue
+        for i,c in enumerate(name):
+            if c == "." or c == "_": 
+                folderName = name[:i]
+                break
+        if folderName == "":
+            folderName = "uncategorized"
+        if folderName == "sub":
+            folderName = "unnamed"
+        folders.setdefault(folderName,[]).append(name)
+    for folderName in folders:
+        if func_dir.isdir(folderName):
+            continue
+        else:
+            try:
+                func_dir.mkdir(folderName)
+            except:
+                print("Failed to create folder: ", folderName)
+                continue
+
+def pre_ida770():
+        for name in folders[folderName]:
+            try:
+                func_dir.rename(name, folderName+"/"+name)
+            except:
+                print("Failed to move function:", name)
+
+def folder_sort_and_nest():
+    common_packages = ["archive", "bufio", "builtin", "bytes", "compress", "container", "context", "crypto", "database", "debug", "embed", "encoding", "errors", "flag", "fmt", "go", "hash", "html", "image", "index", "io", "log", "math", "mime", "net", "os", "path", "plugin", "regexp", "sort", "strconv", "strings", "sync", "syscall", "testing", "text", "time", "unicode", "unsafe", "internal", "reflect", "vendor", "golang", "runtime", "type", "setg", "pthread", "walk"]
+    folderName = "StandardGoPackages"
     try:
         func_dir.mkdir(folderName)
+        for package in common_packages:
+            try:
+                func_dir.rename(package, folderName+"/"+package)
+            except:
+                print("Failed to move standard package folder:", package)
     except:
         print("Failed to create folder: ", folderName)
-        continue
-    for name in folders[folderName]:
-        try:
-            func_dir.rename(name, folderName+"/"+name)
-            #print("Moved: "+name+" to "+folderName)
-        except:
-            print("Failed to move function:", name)
-
-common_packages = ["archive", "bufio", "builtin", "bytes", "compress", "container", "context", "crypto", "database", "debug", "embed", "encoding", "errors", "flag", "fmt", "go", "hash", "html", "image", "index", "io", "log", "math", "mime", "net", "os", "path", "plugin", "regexp", "sort", "strconv", "strings", "sync", "syscall", "testing", "text", "time", "unicode", "unsafe", "internal", "reflect", "vendor", "golang", "runtime", "type", "setg", "pthread"]
-
-folderName = "StandardGoPackages"
-
-try:
-    func_dir.mkdir(folderName)
-    for package in common_packages:
-        try:
-            func_dir.rename(package, folderName+"/"+package)
-        except:
-            print("Failed to move standard package folder:", package)
-except:
-    print("Failed to create folder: ", folderName)
 
 
-if "github" in folders:
+def github_sort():
     github_repos = {}
     for package in folders["github"]:
         repo = ""
@@ -76,3 +82,24 @@ if "github" in folders:
                 func_dir.rename("github/"+func, sub_folder+"/"+func)
             except:
                 print("Failed to move github repo:", func)
+
+def handle_unnested():
+    unnested = {"uncategorized", "unnamed", "nullsub"}
+    for key in unnested:
+        if folders.get(key) is None:
+            continue
+        print(folders[key]) #DEBUG
+        for name in folders[key]:
+            try:
+                func_dir.rename(name, key+"/"+name)
+            except:
+                print("Failed to move function:", name)
+
+populate_folders()
+if idaapi.IDA_SDK_VERSION < 770:
+    pre_ida770()
+folder_sort_and_nest()
+github_sort()
+handle_unnested()
+
+#Still need to handle a few stragglers from the conflicting sorting mechanism introduced in IDA 7.7 but this is much better :)
--- 5.extract_types.py
+++ 5.extract_types.py
@@ -59,72 +59,72 @@ struct golang_type
 """
 
 def find_type_structures(func_name, search_len=15):
-	"""
-	Looks for all types passed as argument to the given function. Probably only
-	works for Go > 1.15 where the register calling convention was introduced.
-	
-	func_name: The name of the function to look for (i.e. "runtime.newobject")
-	register: The register in which the required argument is passed (i.e. "eax")
-	"""
-	type_addresses = set()
-	# Find all xrefs to the given function
-	for f in Functions():
-		if ida_funcs.get_func_name(f) == func_name:
-			for ref in XrefsTo(f):
-				# Find the type argument of that function
-				for h in Heads(ref.frm - search_len, ref.frm):
-					print(f"Instruction: {hex(h)} - {print_insn_mnem(h)}")
-					if "lea" == print_insn_mnem(h) and (get_operand_type(h, 1) == o_imm or get_operand_type(h, 1) == o_mem) and (print_operand(h, 0) == "rcx" or print_operand(h, 0) == "rax"):
-						print("FOUND")
-						type_addresses.add(get_operand_value(h, 1))
-						break
-			break
-	return type_addresses
+    """
+    Looks for all types passed as argument to the given function. Probably only
+    works for Go > 1.15 where the register calling convention was introduced.
+    
+    func_name: The name of the function to look for (i.e. "runtime.newobject")
+    register: The register in which the required argument is passed (i.e. "eax")
+    """
+    type_addresses = set()
+    # Find all xrefs to the given function
+    for f in Functions():
+        if ida_funcs.get_func_name(f) == func_name:
+            for ref in XrefsTo(f):
+                # Find the type argument of that function
+                for h in Heads(ref.frm - search_len, ref.frm):
+                    print(f"Instruction: {hex(h)} - {print_insn_mnem(h)}")
+                    if "lea" == print_insn_mnem(h) and (get_operand_type(h, 1) == o_imm or get_operand_type(h, 1) == o_mem) and (print_operand(h, 0) == "rcx" or print_operand(h, 0) == "rax"):
+                        print("FOUND")
+                        type_addresses.add(get_operand_value(h, 1))
+                        break
+            break
+    return type_addresses
 
 
 def parse_type(addr):
-	"""
-	Applies the correct structure to the type at the given address and locates its name.
-	"""
-	SetType(addr, "golang_type")
-	data_addr = -1
-	for s in Segments():
-		if get_segm_name(s) == ".rdata":
-		   data_addr = get_segm_start(s)
-	if data_addr == -1:
-		print("Could not find .rdata segment!")
-		return False
-		   
-	# nameOff is an offset into rdata. We end up on a structure where the first byte is a bitfield
-	# followed by the size of the string followed by the name of the type.
-	# https://github.com/golang/go/blob/release-branch.go1.16/src/reflect/type.go#L443
-	nameOff = get_wide_dword(addr + 0x28) + data_addr
-	if nameOff == data_addr:
-		return True  # No type string, just move on
-		
-	# Starting from Go 1.17 (?), the size is provided as a varint-encoded length.
-	size = get_wide_byte(nameOff + 1) << 8 | get_wide_byte(nameOff + 2)
-	if size > 0xFF:  # Quick & dirty sanity check.
-		size = get_wide_byte(nameOff + 1)  # This is almost certain to break eventually
-		type_str = get_strlit_contents(nameOff + 2, size)
-	else:
-		type_str = get_strlit_contents(nameOff + 3, size)
-	if not type_str:
-		print(f"Could not obtain type name for {hex(addr)} at address {hex(nameOff)}")
-		del_items(addr)  # Was probably a FP, delete the structure and move on
-		return True
-	set_cmt(addr, type_str.decode(errors="replace"), False)
-	for ref in XrefsTo(addr):
-		set_cmt(ref.frm, type_str.decode(errors="replace"), False)
-	# Rename the structure too. 0x800 = SN_FORCE, not available for some reason
-	# See https://hex-rays.com/products/ida/support/idadoc/203.shtml
-	set_name(addr, "type_" + type_str.decode(errors="replace")[:20], SN_NOCHECK | 0x800)
-	return True
+    """
+    Applies the correct structure to the type at the given address and locates its name.
+    """
+    SetType(addr, "golang_type")
+    data_addr = -1
+    for s in Segments():
+        if (get_segm_name(s) == ".rdata") or (get_segm_name(s) == "__rodata"):
+           data_addr = get_segm_start(s)
+    if data_addr == -1:
+        print("Could not find .rdata segment!")
+        return False
+           
+    # nameOff is an offset into rdata. We end up on a structure where the first byte is a bitfield
+    # followed by the size of the string followed by the name of the type.
+    # https://github.com/golang/go/blob/release-branch.go1.16/src/reflect/type.go#L443
+    nameOff = get_wide_dword(addr + 0x28) + data_addr
+    if nameOff == data_addr:
+        return True  # No type string, just move on
+        
+    # Starting from Go 1.17 (?), the size is provided as a varint-encoded length.
+    size = get_wide_byte(nameOff + 1) << 8 | get_wide_byte(nameOff + 2)
+    if size > 0xFF:  # Quick & dirty sanity check.
+        size = get_wide_byte(nameOff + 1)  # This is almost certain to break eventually
+        type_str = get_strlit_contents(nameOff + 2, size)
+    else:
+        type_str = get_strlit_contents(nameOff + 3, size)
+    if not type_str:
+        print(f"Could not obtain type name for {hex(addr)} at address {hex(nameOff)}")
+        del_items(addr)  # Was probably a FP, delete the structure and move on
+        return True
+    set_cmt(addr, type_str.decode(errors="replace"), False)
+    for ref in XrefsTo(addr):
+        set_cmt(ref.frm, type_str.decode(errors="replace"), False)
+    # Rename the structure too. 0x800 = SN_FORCE, not available for some reason
+    # See https://hex-rays.com/products/ida/support/idadoc/203.shtml
+    set_name(addr, "type_" + type_str.decode(errors="replace")[:20], SN_NOCHECK | 0x800)
+    return True
 
 
 # Import the required IDA structures if necessary
 if get_struc_id("golang_type") == BADADDR:
-	parse_decls(C_HEADER, idaapi.PT_TYP)
+    parse_decls(C_HEADER, idaapi.PT_TYP)
 
 # Find all places in the binary where there is type information
 addresses  = find_type_structures("runtime.newobject")
@@ -135,5 +135,5 @@ addresses |= find_type_structures("runtime.makeslice", search_len=30)
 
 # Parse type information
 for t in addresses:
-	if not parse_type(t):
-		break  # Stop on first fatal error
+    if not parse_type(t):
+        break  # Stop on first fatal error
