f2c169c24292449d5774cc2880c45be3fb165ee1
turla <jags.sec@gmail.com>
2022-05-16 20:25:52 -0400

    Massively reorged to support 7.7 and fix some shitty edgecases

--- 3.categorize_go_folders.py
+++ 3.categorize_go_folders.py
@@ -10,12 +10,11 @@ func_dir =   ida_dirtree.get_std_dirtree(ida_dirtree.DIRTREE_FUNCS)
 ite = ida_dirtree.dirtree_iterator_t()
 ok = func_dir.findfirst(ite, "*")
 
-version770plus = False
 folders = {}
-folderName = ""
 
-def populate_folders():
+def create_folders():
     for function in idautils.Functions():
+        folderName = ""
         name = idc.get_func_name(function)
         if name.startswith("_") or name.startswith("."):
             folders.setdefault("uncategorized",[]).append(name)
@@ -29,25 +28,46 @@ def populate_folders():
         if folderName == "sub":
             folderName = "unnamed"
         folders.setdefault(folderName,[]).append(name)
+    existed = []
+    created = []
     for folderName in folders:
         if func_dir.isdir(folderName):
+            existed.append(folderName)
             continue
         else:
             try:
                 func_dir.mkdir(folderName)
+                created.append(folderName)
             except:
                 print("Failed to create folder: ", folderName)
                 continue
+    for folderName in existed:
+        if (folderName != "github"):
+            folders.pop(folderName)
+    for folder in created:
+        if len(folders[folder]) <= 3:
+            for func in folders[folder]:
+                folders.setdefault("uncategorized",[]).append(func)
+            folders.pop(folder)
+            func_dir.rmdir(folder)
 
-def pre_ida770():
-        for name in folders[folderName]:
+def populate_folders():
+    for folderName in folders:
+        if not func_dir.isdir(folderName):
+            print("Error: %s folder didn't exist, recreating...", folderName)
             try:
-                func_dir.rename(name, folderName+"/"+name)
+                func_dir.mkdir(folderName)
             except:
-                print("Failed to move function:", name)
+                print("Couldn't create folder: " + folderName)
+        list_of_funcs = folders[folderName]
+        for func in list_of_funcs:
+            try:
+                func_dir.rename(func, folderName+"/"+func)
+            except:
+                print("Failed to move function:", func)
 
-def folder_sort_and_nest():
-    common_packages = ["archive", "bufio", "builtin", "bytes", "compress", "container", "context", "crypto", "database", "debug", "embed", "encoding", "errors", "flag", "fmt", "go", "hash", "html", "image", "index", "io", "log", "math", "mime", "net", "os", "path", "plugin", "regexp", "sort", "strconv", "strings", "sync", "syscall", "testing", "text", "time", "unicode", "unsafe", "internal", "reflect", "vendor", "golang", "runtime", "type", "setg", "pthread", "walk"]
+def nest_standard_packages():
+    common_packages = ["archive", "bufio", "builtin", "bytes", "compress", "container", "context", "crypto", "database", "debug", "embed", "encoding", "errors", "flag", "fmt", "go", "hash", "html", "image", "index", "io", "log", "math", "mime", "net", "os", "path", "plugin", "regexp", "sort", "strconv", "strings", "sync", "syscall", "testing", "text", "time", "unicode", "unsafe", "internal", "reflect", "vendor", "golang", "runtime", "type", "setg", "pthread", "walk", "gosave", "x", "cgo"]
     folderName = "StandardGoPackages"
     try:
         func_dir.mkdir(folderName)
@@ -83,23 +103,9 @@ def github_sort():
             except:
                 print("Failed to move github repo:", func)
 
-def handle_unnested():
-    unnested = {"uncategorized", "unnamed", "nullsub"}
-    for key in unnested:
-        if folders.get(key) is None:
-            continue
-        print(folders[key]) #DEBUG
-        for name in folders[key]:
-            try:
-                func_dir.rename(name, key+"/"+name)
-            except:
-                print("Failed to move function:", name)
-
+create_folders()
 populate_folders()
-if idaapi.IDA_SDK_VERSION < 770:
-    pre_ida770()
-folder_sort_and_nest()
+nest_standard_packages()
 github_sort()
-handle_unnested()
 
 #Still need to handle a few stragglers from the conflicting sorting mechanism introduced in IDA 7.7 but this is much better :)
